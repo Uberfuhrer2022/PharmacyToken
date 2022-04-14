b22c85e8c27238483b5fb69c841b708cb4fdf07c
Pizza Knight <103531316+Uberfuhrer2022@users.noreply.github.com>
2022-04-14 15:14:09 +0400

    Create TokenGenTest.py
    Functional JSON writer and reader.
    
    Issues:
    1) Date/Time returns as 'None' from function.
    2) Date/Time returns as 'Null' when read from JSON file.
    
    Date/Time code works outside the function though. Tried declaring and import module both inside and outside function, but it doesn't work.

---
+++ Testing/TokenGenTest.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import json
+from datetime import datetime
+
+
+def TokenCreate():
+    CurrentDay = datetime.today()
+    CurrentTime = datetime.now()
+
+    tknDate = CurrentDay.strftime("%d-%b-%Y")
+    tknTime = CurrentTime.strftime("%H:%M:%S")
+
+    data = {
+        "TokenID": 0,
+        "Time": print(tknTime),
+        "Date": print(tknDate)
+    }
+
+    print(f"Data:", data)
+
+    # printing object as json string
+    s = json.dumps(data)
+    print(f"JSON Input: ", s)
+
+    # getting python object from json string
+    data2 = json.loads(s)
+    assert data2 == data
+
+    # writing data to file
+    with open('test_data.json', 'w') as f:
+        json.dump(data, f)
+
+    # reading data from file
+    with open('test_data.json') as f:
+        data3 = json.load(f)
+    assert data3 == data
+
+
+if __name__ == '__main__':
+    TokenCreate()
