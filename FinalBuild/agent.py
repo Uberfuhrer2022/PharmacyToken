99c9d8e558327ae8b93a2c818f041a89f699abd2
Pizza Knight <103531316+Uberfuhrer2022@users.noreply.github.com>
2022-04-25 23:17:20 +0400

    Update agent.py
    Updates:
     - Functioning Counter w/ Date and Time
     - Token information
     - Queue Length
    
    Second multiline box takes data from /report.

--- FinalBuild/agent.py
+++ FinalBuild/agent.py
@@ -1,3 +1,6 @@
+from server import tokDate, tokTime
+import json
+import requests
 from ctypes.wintypes import SIZE
 from lib2to3.pgen2.token import NEWLINE
 from tkinter import CENTER, Scale
@@ -30,7 +33,11 @@ col_SERVICE = [
 
 col_COUNTER = [
     [app.Text('CUSTOMER DETAILS', font=('Arial', 10, 'bold'))],
-    [app.Multiline('Request a new customer to start.', size=(60, 20), key=('-QUEUE-'),font=('Courier New', 18))]
+    [app.Multiline('Request a new customer to start.', size=(
+        60, 10), key=('-QUEUE-'), font=('Courier New', 12))],
+    [app.Text('QUEUE COUNTER PANEL', font=('Arial', 10, 'bold'))],
+    [app.Multiline(f'|-------------------------- {tokDate} --|-- {tokTime} ----|', size=(
+        60, 6), key=('-Q_COUNTER-'), font=('Tahoma', 12))]
 ]
 
 layout = [
@@ -42,7 +49,7 @@ layout = [
 
 # Application Window
 window = app.Window("ASTER PHARMACY - Employee Control Board", layout, size=(
-    500, 550))  # ,element_justification='center'
+    700, 550))  # ,element_justification='center'
 
 
 # AgentValue: 200 (REQUESTING CUSTOMER), 400 (ENDING SERVICE)
@@ -50,15 +57,6 @@ window = app.Window("ASTER PHARMACY - Employee Control Board", layout, size=(
 
 AgentValue = 0
 DutyAbility = False
-
-
-########### FastAPI Config ###########
-
-import requests
-import json
-
-######################################
-
 
 
 while True:
@@ -75,20 +73,78 @@ while True:
         while DutyAbility == True:
             AgentValue = 200
             print(AgentValue)
-            
+
             api_url = 'http://localhost:8000/get_token'
             response = requests.get(api_url)
             print(response)
-            
+
             data = response.text
             parse_json = json.loads(data)
             print(parse_json)
-            
+
             text = window['-QUEUE-']
             window.FindElement('-QUEUE-').Update('')
-            text.update(text.get()+f"\n{str(parse_json)}")
-            
-            
+
+            # FULL DATA LENGTH
+            # text.update(text.get()+f"\n{str(parse_json)}")
+            remove_chars = "('),"
+
+            # Token Number
+            token_number = parse_json[0]
+            tokNumRow = ('Token Number: ',
+                         token_number['Token Number'])  # Token Number
+            new_tokNum = str(tokNumRow)
+
+            for chars in remove_chars:
+                new_tokNum = new_tokNum.replace(chars, '')
+            text.update(text.get()+f"\n{str(new_tokNum)}")
+
+            # Token ID
+            token_ID = parse_json[0]
+            tokIDRow = ('Token ID: ',
+                        token_ID['Token ID'])  # Token ID
+            new_tokID = str(tokIDRow)
+
+            for chars in remove_chars:
+                new_tokID = new_tokID.replace(chars, '')
+            text.update(text.get()+f"\n{str(new_tokID)}")
+
+            # Customer Queue Length
+            queue_number = parse_json[1]
+            queNumRow = ('Number of Customers in Queue: ',
+                         queue_number['Number of Customers in Queue'])
+            new_queNum = str(queNumRow)
+
+            for chars in remove_chars:
+                new_queNum = new_queNum.replace(chars, '')
+            text.update(text.get()+f"\n\n\n\n{str(new_queNum)}")
+
+            # ------| CUSTOMER COUNTER BELOW! | ------ #
+            api_url = 'http://localhost:8000/report'
+            response = requests.get(api_url)
+            print(response)
+
+            data = response.text
+            parse_json = json.loads(data)
+            print(parse_json)
+
+            text2 = window['-Q_COUNTER-']
+            # window.FindElement('-Q_COUNTER-').Update('') <-- clears text before appending
+
+            # FULL DATA LENGTH
+            # text.update(text.get()+f"\n{str(parse_json)}")
+            remove_chars = "('),"
+
+            # Token Number
+            last_token = parse_json
+            q_Number = ('Token Number: ',
+                        last_token['Token Number'])  # Last Token
+            newq_Number = str(q_Number)
+
+            for chars in remove_chars:
+                newq_Number = newq_Number.replace(chars, '')
+            text2.update(text2.get()+f"\n{str(newq_Number)}")
+
             break
         else:
             print('ERROR: Please start your shift before serving customers.')
@@ -97,6 +153,7 @@ while True:
         while DutyAbility == True:
             AgentValue = 400
             print(AgentValue)
+            window.close()
             break
         else:
             print('ERROR: Please start your shift before serving customers.')
